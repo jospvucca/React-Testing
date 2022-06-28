605700164e96c31478d2457ced266e793b1b5414
Josip Vučica <jv.vucica@gmail.com>
2022-06-28 22:04:53 +0200

    latest

--- src/Parent.js
+++ src/Parent.js
@@ -5,6 +5,8 @@ import LobbyPage from "./LobbyPage";
 import WritingPhase from "./WritingPhase";
 import { withNavigate } from "./hoc/withNavigate";
 import SignalRHubConnector from "./signalr/SignalRHubConnector";
+import * as SignalR from "@microsoft/signalr";
+
 export class Parent extends React.Component {
   constructor(props) {
     super(props);
@@ -14,12 +16,55 @@ export class Parent extends React.Component {
     };
   }
 
+  instantiateSignalRConnection = async () => {
+    const hubConnection = new SignalR.HubConnectionBuilder()
+      .withUrl("http://localhost:43358/api/Lobby/create", {
+        // skipNegotiation: true,
+        transport: SignalR.HttpTransportType.WebSockets,
+        connectionId: "oCiGPUtYeDh8y3ee8edCWS",
+      })
+      .configureLogging(SignalR.LogLevel.Information)
+      // .withAutomaticReconnect()
+      .withHubProtocol(new SignalR.JsonHubProtocol())
+      .build();
+
+    await this.setState(
+      { hubConnection: hubConnection },
+      async () =>
+        await this.state.hubConnection
+          .start()
+          .then(() => console.log("Connection Started!"))
+          .catch(err => console.error(err))
+    );
+
+    // if (
+    //   hubConnection.connectionId !== null ||
+    //   hubConnection.connectionId === null
+    // ) {
+    //   console.error(hubConnection.connectionId);
+    // }
+
+    // this.props.signalRPropsCallback(hubConnection);
+
+    this.state.hubConnection.on(
+      "ReceiveMessage",
+      (receivedUsername, receivedMessage) => {
+        console.log(
+          "HubConnection recieved method xxxReceiveMessagexxx with parameters: " +
+            receivedUsername +
+            " " +
+            receivedMessage
+        );
+      }
+    );
+  };
+
   signalRPropsCallbackFunction = async hub => {
     console.warn(
       "Parent::signalRPropsCallbackFunction... This function should recieve all props from signalr class and be used as a main here"
     );
     console.warn("Recieved: " + JSON.stringify(hub));
-    await this.setState({ hubConnection: hub });
+    // await this.setState({ hubConnection: hub });
     console.warn("UPDATED HUB: " + JSON.stringify(this.state.hubConnection));
   };
 
@@ -46,15 +91,21 @@ export class Parent extends React.Component {
       .catch(err => console.error(err));
   };
 
-  componentDidMount() {}
+  componentDidMount = async () => {
+    if (this.state.hubConnection == null) {
+      await this.instantiateSignalRConnection();
+    } else {
+      console.log("Parent::componentDidMount::hubConnection is not null.");
+    }
+  };
 
   render() {
     return (
       <div>
         Signalr: {JSON.stringify(this.state.hubConnection)}
-        <SignalRHubConnector
+        {/* <SignalRHubConnector
           signalRPropsCallback={this.signalRPropsCallbackFunction}
-        />
+        /> */}
         <Routes>
           <Route
             exact
@@ -63,7 +114,7 @@ export class Parent extends React.Component {
               <div>
                 <LoginPage
                   parentCallback={this.callbackFunction}
-                  signalRParentCallback={this.signalRParentCallbackFunction}
+                  // signalRParentCallback={this.signalRParentCallbackFunction}
                 />
                 Data: {JSON.stringify(this.state.hubConnection)}
               </div>
